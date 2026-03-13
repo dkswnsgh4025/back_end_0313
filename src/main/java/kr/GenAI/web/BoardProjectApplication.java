528cf90ccfff1ed9bd04ca89084795ec859b5b89
dkswnsgh4025 <dkswnsgh4025@gmail.com>
2026-03-13 10:35:00 +0900

    update

---
+++ src/main/java/kr/GenAI/web/BoardProjectApplication.java
@@ -0,0 +1,14 @@
+package kr.GenAI.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import kr.GenAI.web.BoardProjectApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectApplication.class, args);
+	}
+}
